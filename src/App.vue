25c9e638baee393a1d761170791864b408f90376
margallostephen <spmargallo@gmail.com>
2024-03-18 05:06:18 +0800

    Add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="justify-center flex bg-blue-300 items-center h-screen">
+    <div class="text-4xl">Hello Welcome to Tailwind Css</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
