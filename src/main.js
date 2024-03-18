88d3886c305c84ba80c6a01630ad62a8ad87bb4b
margallostephen <spmargallo@gmail.com>
2024-03-18 18:44:41 +0800

    Add icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCartPlus, faCheck, faShoppingCart, faTrashAlt, faClose } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app')
+library.add(faCheck, faTrashAlt, faShoppingCart, faCartPlus, faClose);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
