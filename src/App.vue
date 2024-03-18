68604363161699e3b2b12488581164a26ae1f152
margallostephen <spmargallo@gmail.com>
2024-03-18 19:10:45 +0800

    Update App Vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div class="justify-center flex bg-blue-300 items-center h-screen">
-    <div class="text-4xl">Hello Welcome to Tailwind Css</div>
+  <div>
+    <ProductList
+      :products="products"
+      :cart="cart"
+      @add-to-cart="addToCart"
+    ></ProductList>
+    <ShoppingCart 
+      :cart="cart" 
+      @remove="removeFromCart">
+    </ShoppingCart>
   </div>
 </template>
 
 <script>
+import ProductList from "./components/ProductList.vue";
+import ShoppingCart from "./components/ShoppingCart.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { ProductList, ShoppingCart },
+  data() {
+    return { products: [], cart: [] };
+  },
+  async created() {
+    try {
+      const response = await fetch(`https://dummyjson.com/products?limit=10`);
+      if (!response.ok) throw new Error("Failed to fetch products");
+      this.products = (await response.json()).products;
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  },
+
+  methods: {
+    addToCart(product) {
+      if (!this.cart.find((item) => item.id === product.id))
+        this.cart.push({ ...product, quantity: 1 });
+    },
+    removeFromCart(index) {
+      this.cart.splice(index, 1);
+    },
+  },
 };
 </script>
-
